dbd19a1b7f7604a8846562ec3b9a6bfdaf0b3e5c
NikitaSpirin321234 <nikitanikita04062000@gmail.com>
2026-04-12 14:05:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
+
+class Note extends Model {}
+Note.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  important: {
+    type: DataTypes.BOOLEAN
+  },
+  date: {
+    type: DataTypes.DATE
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'note'
+})
+
+Note.sync()
+
+app.use(express.json())
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await Note.findAll()
+
+  console.log(JSON.stringify(notes))
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    console.log(note.toJSON())    
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.put('/api/notes/:id', async (req, res) => {
+  const note = await Note.findByPk(req.params.id)
+  if (note) {
+    note.important = req.body.important
+    await note.save()
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    console.log("req.body: ", req.body);
+    const note = await Note.create({...req.body, date: new Date()})
+    return res.json(note)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
